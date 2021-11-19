9db4dc161f2b2a27f6d29da5e61e7a471e69f668
Jasper Krom <jasper.krom@gmail.com>
2021-11-19 15:22:53 +0100

    Memstream failure scenario

--- IronPDFTestCaseApp/Program.cs
+++ IronPDFTestCaseApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using IronPdf;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace IronPDFTestCaseApp
@@ -12,19 +13,33 @@ namespace IronPDFTestCaseApp
 
             Console.WriteLine("Please insert file location of Pdf File:");
             var fileLocation = Console.ReadLine();
-            var pdf = new PdfDocument(fileLocation);
+            //var pdf = new PdfDocument(fileLocation);
+            using (var stream = new FileStream(fileLocation, FileMode.Open))
+            {
 
-            var ForegroundStamp = new IronPdf.Editing.HtmlStamp() { Html = "<h2 style='color:red'>Just some text", 
-                Width = 50, Height = 50, Opacity = 50, 
-                ZIndex = IronPdf.Editing.HtmlStamp.StampLayer.OnTopOfExistingPDFContent };
-            await pdf.StampHTMLAsync(ForegroundStamp);
+                using (var memStream = new MemoryStream())
+                {
+                    stream.CopyTo(memStream);
+                    var pdf = new PdfDocument(stream);
 
-            Console.WriteLine("Please insert location to save Pdf file:");
-            var saveLocation = Console.ReadLine();
-            pdf.SaveAs(saveLocation);
+                    var ForegroundStamp = new IronPdf.Editing.HtmlStamp()
+                    {
+                        Html = "<h2 style='color:red'>Just some text",
+                        Width = 50,
+                        Height = 50,
+                        Opacity = 50,
+                        ZIndex = IronPdf.Editing.HtmlStamp.StampLayer.OnTopOfExistingPDFContent
+                    };
+                    await pdf.StampHTMLAsync(ForegroundStamp);
 
-            Console.WriteLine("File saved");
-            Console.ReadLine();
+                    Console.WriteLine("Please insert location to save Pdf file:");
+                    var saveLocation = Console.ReadLine();
+                    pdf.SaveAs(saveLocation);
+
+                    Console.WriteLine("File saved");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
